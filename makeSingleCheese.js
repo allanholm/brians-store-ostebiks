57b2b531f377039580625430ed1548f844916b63
Allan Holm Søndergaard <6605392@rts-365.dk>
2020-12-18 09:59:10 +0100

    styling

--- makeSingleCheese.js
+++ makeSingleCheese.js
@@ -6,16 +6,16 @@ function createSingleCheese(cheese){
   cheeseName.className = "cheeseName";
 
   let cheeseBrand = document.createElement("p");
-  cheeseBrand.innerText = "Brand: " + cheese.brand;
+  cheeseBrand.innerText = "Fabrikant: " + cheese.brand;
 
   let cheesePrice = document.createElement("p");
-  cheesePrice.innerText = "Price: " + cheese.price.$numberDecimal + " ,-";
+  cheesePrice.innerText = "Pris: " + cheese.price.$numberDecimal + " ,-";
 
   let cheeseWeight = document.createElement("p");
-  cheeseWeight.innerText = "Weight: " + cheese.weight + " gram";
+  cheeseWeight.innerText = "Vægt: " + cheese.weight + " gram";
 
   let cheeseStrength = document.createElement("p");
-  cheeseStrength.innerText = "Strength: " + cheese.strength;
+  cheeseStrength.innerText = "Styrke: " + cheese.strength;
 
   cheeseContainer.append(cheeseName, cheeseBrand, cheesePrice, cheeseWeight, cheeseStrength);
 }
